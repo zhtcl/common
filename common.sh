13825d6fba9515ed62ea0197609310d0605e3886
zhtcl <110948436+zhtcl@users.noreply.github.com>
2024-04-01 06:38:50 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -326,7 +326,7 @@ z="*luci-theme-argone*,*luci-theme-argon*,*luci-app-argon-config*,*luci-app-argo
 luci-theme-rosy,luci-theme-darkmatter,luci-theme-infinityfreedom,luci-theme-design,luci-app-design-config, \
 luci-theme-bootstrap-mod,luci-theme-freifunk-generic,luci-theme-opentomato,luci-theme-kucat, \
 luci-app-openclash,luci-app-chinesesubfinder,luci-app-bmtedge,luci-app-bypass,miniupnpd,luci-app-torbp,*luci-app-passwall*,luci-app-passwall,luci-app-passwall2, \
-luci-app-ssr-plus"
+luci-app-clouddrive2,luci-app-ssr-plus"
 #luci-app-eqos,adguardhome,luci-app-adguardhome,mosdns,luci-app-mosdns,luci-app-wol,luci-app-openclash, \
 #luci-app-gost,gost,luci-app-smartdns,smartdns,luci-app-wizard,luci-app-msd_lite,msd_lite, \
 #luci-app-ssr-plus,*luci-app-passwall*,luci-app-passwall,luci-app-passwall2,luci-app-bypass,luci-app-vssr,lua-maxminddb,v2dat,v2ray-geodata, \
