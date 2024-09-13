e84f31fc71b05b47ae6dc17dea796bd69f8b0a9f
zhtcl <110948436+zhtcl@users.noreply.github.com>
2024-09-14 06:21:01 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -498,9 +498,14 @@ done
 rm -rf ${HOME_PATH}/feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x ${HOME_PATH}/feeds/packages/lang/golang
 # r2s注释
+case "${SOURCE_CODE}" in
+COOLSNOWWOLF)
 if [[ -d "${HOME_PATH}/package/boot/uboot-rk35xx/Makefile" ]]; then
 sed -i '/^UBOOT_TARGETS := rk3528-evb rk3588-evb/s/^/#/' package/boot/uboot-rk35xx/Makefile
 fi
+;;
+esac
+
 # 更换luci-theme-argon
 #rm -rf ${HOME_PATH}/feeds/danshui2/luci-theme-argon
 #git clone https://github.com/jerrykuku/luci-theme-argon -b master ${HOME_PATH}/feeds/danshui2/luci-theme-argon
