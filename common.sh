d6329ed78fb35bfed2337a59dbe084f76e7c595f
zhtcl <110948436+zhtcl@users.noreply.github.com>
2025-03-15 18:21:23 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -310,7 +310,7 @@ mv -f uniq.conf feeds.conf.default
 
 # 这里增加了源,要对应的删除/etc/opkg/distfeeds.conf插件源
 cat >>"feeds.conf.default" <<-EOF
-src-git smpackage https://github.com/kenzok8/openwrt-packages;main
+src-git smpackage https://github.com/kenzok8/openwrt-packages;master
 #src-git danshui1 https://github.com/281677160/openwrt-package.git;${SOURCE}
 src-git helloworld https://github.com/fw876/helloworld.git
 src-git passwall3 https://github.com/xiaorouji/openwrt-passwall-packages;main
